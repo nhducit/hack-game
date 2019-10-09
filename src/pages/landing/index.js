ac2d4731081a7952d1adda48736afdf4517bbdcb
Long Lee <longhoangly@gmail.com>
2019-10-09 08:27:36 +0700

    load game page when click on play button

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,59 +1,45 @@
-import Head from "next/head";
 import React from "react";
-import posed from "react-pose";
-import moment from "moment";
 import Wrapper from "../../components/wrapPage";
 import { images } from "../../assets";
-import Game from "./../game";
-
-
-const land =
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "flex-end",
-      alignItems: "center",
-      width: "100%",
-      height: "100%",
-      backgroundImage: `url(${images.bgIntro()})`,
-      backgroundSize: "contain",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center"
-    }}
-  >
-    <img
-      style={{
-        width: "95%",
-        height: "auto",
-        bottom: "15px",
-        marginBottom: "30px",
-        cursor: "pointer"
-      }}
-      src={images.playBtn()}
-      onClick={() => {
-        console.log('tapped');
-
-        function handleClick(e) {
-          e.preventDefault();
-          console.log('User wanna plays game lah!');
-        }
-
-        return (
-          <Game />
-        );
-      }} />
-  </div>
-
 
 class Landing extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  land =
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        backgroundImage: `url(${images.bgIntro()})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center"
+      }}
+    >
+      <img
+        style={{
+          width: "95%",
+          height: "auto",
+          bottom: "15px",
+          marginBottom: "30px",
+          cursor: "pointer"
+        }}
+        src={images.playBtn()}
+        onClick={() => {
+          console.log('A user wanna plays game lah!');
+          document.location.href = "game";
+        }} />
+    </div>
+
   render = () => {
     return (
-      <Wrapper bg={images.bgIntro()} children={land}></Wrapper>
+      <Wrapper bg={images.bgIntro()} children={this.land}></Wrapper>
     );
   };
 }
